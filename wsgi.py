951157c94677c32428c947a689779795b4829f6d
Robert Seward <rseward@bluestone-consulting.com>
2018-10-04 14:30:23 -0400

    namaste edition.

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,9 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    #greeting="Hello World!"
+    greeting="namste duniya"
+    return greeting
 
 if __name__ == "__main__":
     application.run()
